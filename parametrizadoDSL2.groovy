a59fa52c506ec0022e9144528e0a7bb0a68ba9e4
Luis Suclupe <lsuclupe@gmail.com>
2023-07-26 19:01:26 -0500

    Modificación para datos de Luis Suclupe

--- parametrizadoDSL2.groovy
+++ parametrizadoDSL2.groovy
@@ -1,9 +1,9 @@
 job('ejemplo3-job-DSL') {
 	description('Job DSL de ejemplo para el curso de Jenkins')
   	scm {
-      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
+      		git('https://github.com/lsuclupe01/jenkinsCurso.git', 'main') { node ->
+        		node / gitConfigName('lsuclupe01')
+        		node / gitConfigEmail('lsuclupe@gmail.com')
       		}
     	} 
   	parameters {
@@ -19,7 +19,7 @@ job('ejemplo3-job-DSL') {
     		shell("bash jobscript.sh")
     	}
   	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
+      		mailer('lsuclupe@gmail.com', true, true)
       		slackNotifier {
 		  notifyAborted(true)
 		  notifyEveryFailure(true)
